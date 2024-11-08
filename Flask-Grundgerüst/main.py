1daa4d6ebb05f8536c62ea8dde65d6edc9610afc
Nils Generlich <nils.generlich@stud.htw-dresden.de>
2024-11-08 02:21:47 +0100

    Tolerance in Prozent anstatt Varianz-Parameter

--- Flask-Grundgerüst/main.py
+++ Flask-Grundgerüst/main.py
@@ -1,6 +1,6 @@
 # main.py
 from flask import Blueprint, render_template, request, jsonify, session, redirect, url_for
-from model import simulate_risk_matrix, plot_priority_distribution, heatmap_svg, scatter_svg
+from model import simulate_risk_matrix, plot_priority_distribution, heatmap_svg, scatter_svg, conv_perc_std, conv_perc_var
 
 
 main_bp = Blueprint('main', __name__)
@@ -14,9 +14,12 @@ def set_parameters():
     if request.method == 'POST':
         n_simulations = int(request.form['n_simulations'])
         frequency_mean = float(request.form['frequency_mean'])
-        frequency_var = float(request.form['frequency_var'])
+        frequency_perc = float(request.form['frequency_perc'])
         severity_mean = float(request.form['severity_mean'])
-        severity_var = float(request.form['severity_var'])
+        severity_perc = float(request.form['severity_perc'])
+
+    frequency_var = conv_perc_var(95,frequency_perc)# evt. variables Konfidenzintervall eingebbar machen
+    severity_var = conv_perc_var(95,severity_perc)
 
     data = simulate_risk_matrix(n_simulations, frequency_mean, frequency_var, severity_mean, severity_var)
 
