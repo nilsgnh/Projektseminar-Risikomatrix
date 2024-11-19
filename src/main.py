fixed variance bug???

src/main.py
@@ -1,54 +1,30 @@
 # main.py
 from flask import Blueprint, render_template, request
-from model import simulate_risk_matrix, plot_priority_distribution, heatmap_svg, scatter_svg, conv_perc_std, conv_perc_var
 from simulation import *
 from matrix import *
 from plot import *
 
 main_bp = Blueprint('main', __name__)
 
-# Erste Route (set_parameters)
 @main_bp.route('/', methods=['GET'])
 def main():
     return render_template("index.html")
 
-@main_bp.route('/submit2', methods=['GET', 'POST'])
-def set_parameters_v1():
+
+@main_bp.route('/submit', methods=['GET', 'POST'])
+def set_parameters():
     if request.method == 'POST':
         n_simulations = int(request.form['n_simulations'])
         frequency_mean = float(request.form['frequency_mean'])
         frequency_perc = float(request.form['frequency_perc'])
         severity_mean = float(request.form['severity_mean'])
         severity_perc = float(request.form['severity_perc'])
+
 
     frequency_var = conv_perc_var(95, frequency_perc)
     severity_var = conv_perc_var(95, severity_perc)
 
-    data = simulate_risk_matrix(n_simulations, frequency_mean, frequency_var, severity_mean, severity_var)
-
-    frequencies = data[0]
-    severities = data[1]
-    priorities = data[2]
-    matrix_felder = data[3]
-
-    bar_plot = plot_priority_distribution(priorities)
-    heat_plot = heatmap_svg(matrix_felder)
-    scatter_plot = scatter_svg(severity_mean, frequency_mean, priorities, severities, frequencies)
-    return render_template("index.html", plot=bar_plot, plot2=heat_plot, plot3=scatter_plot)
-
-
-# Zweite Route (set_parameters)
-@main_bp.route('/submit', methods=['GET', 'POST'])
-def set_parameters_v2():
-    if request.method == 'POST':
-        n_simulations = int(request.form['n_simulations'])
-        frequency_mean = float(request.form['frequency_mean'])
-        frequency_perc = float(request.form['frequency_perc'])
-        severity_mean = float(request.form['severity_mean'])
-        severity_perc = float(request.form['severity_perc'])
-
-    frequency_var = convertPercentToStandardDeviation(95, frequency_perc)
-    severity_var = convertPercentToVariance(95, severity_perc)
+    print(frequency_var, severity_var)
 
     matrix_rep = np.array([
         [3, 4, 4, 4],   # Häufig
@@ -91,4 +67,5 @@ def set_parameters_v2():
     bar_plot = plotPriorityDistribution(priorities, matrix)
     heat_plot = plotHeatmap(matrix_felder, matrix)
     scatter_plot = plotScatter(severity_mean, frequency_mean, priorities, severities, frequencies, matrix)
+
     return render_template("index.html", plot=bar_plot, plot2=heat_plot, plot3=scatter_plot)

src/simulation.py
@@ -2,11 +2,11 @@ import numpy as np
 from scipy.stats import norm
 
 
-def simulateRiskMatrix(nSimulations, frequencyMean, frequencyVariance, sevserityMean, severityVariance, matrix):
+def simulateRiskMatrix(nSimulations, frequencyMean, frequencyVariance, severityMean, severityVariance, matrix):
     
     # Generate random values, ensuring they are between 0 and 1 (normal distribution)
     frequencies = np.random.normal(frequencyMean, np.sqrt(frequencyVariance), nSimulations)
-    severities = np.random.normal(sevserityMean, np.sqrt(severityVariance), nSimulations)
+    severities = np.random.normal(severityMean, np.sqrt(severityVariance), nSimulations)
     
     # Keep values within [0, 1] range
     frequencies = np.clip(frequencies, 0, 1)
@@ -29,7 +29,7 @@ def simulateRiskMatrix(nSimulations, frequencyMean, frequencyVariance, sevserity
 
 
 
-def convertPercentToStandardDeviation(certainly, percent):
+def conv_perc_std(certainly, percent):
     # z-Quantil für die gegebene Sicherheit berechnen
     z = norm.ppf((certainly / 100) + (1 - (certainly / 100)) / 2)
     
@@ -37,9 +37,7 @@ def convertPercentToStandardDeviation(certainly, percent):
     sigma = (percent / 100) / z
     return sigma
 
-
-
-def convertPercentToVariance(certainly, percent):
+def conv_perc_var(certainly, percent):
     # z-Quantil für die gegebene Sicherheit berechnen
     z = norm.ppf((certainly / 100) + (1 - (certainly / 100)) / 2)
     